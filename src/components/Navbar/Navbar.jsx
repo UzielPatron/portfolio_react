7aa2e54401ac32f331e7e94d45c9708685cc74cf
Uziel Patron <uzielpatron2020@gmail.com>
2022-10-14 21:59:19 -0300

    header section ended

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion';
 
-import images from '../../constants/images';
+import { images } from '../../constants';
 
 import './Navbar.scss';
 
